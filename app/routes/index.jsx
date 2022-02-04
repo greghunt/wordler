fc1556406a30cf7b83d034357dafa9d050a1951d
Greg Hunt <greg@motto.ca>
2022-02-04 13:50:32 -0500

    Temp welcome page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,9 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>A <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> Clone</h1>
+      <p>For groups of friends (wordlers) that like to SPAM their group chats.</p>
+      <p>Coming soon...ish.</p>
     </div>
   );
 }
