6fda1e584140d01ea08160dea755939db3a7eb59
Greg Hunt <greg@motto.ca>
2022-02-05 07:26:37 -0500

    Added TW integration

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div className="bg-slate-500">
       <h1>A <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> Clone</h1>
       <p>For groups of friends (wordlers) that like to SPAM their group chats.</p>
       <p>Coming soon...ish.</p>
