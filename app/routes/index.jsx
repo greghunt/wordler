c3bf92e945709194cdfb71f2f57386b0e6716033
Greg Hunt <greg@motto.ca>
2022-02-05 07:16:46 -0500

    Made game component

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,12 @@
+import Game from '~/components/game'
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>A <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> Clone</h1>
       <p>For groups of friends (wordlers) that like to SPAM their group chats.</p>
       <p>Coming soon...ish.</p>
+      <Game />
     </div>
   );
 }
